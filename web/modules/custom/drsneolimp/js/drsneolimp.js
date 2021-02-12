5450832a86b56d37ffc90965ebabffb8f1ad500c
olzhas.nurpeisov <olzhas.nurpeisov@inessoft.kz>
2021-02-13 00:51:48 +0600

    change sport kinds filter

--- web/modules/custom/drsneolimp/js/drsneolimp.js
+++ web/modules/custom/drsneolimp/js/drsneolimp.js
@@ -1,29 +1,36 @@
 (function($, Drupal, drupalSettings){
 'use strict';
 
- Drupal.behaviors.drsneolimp = {
-   attach: function(context) {
+  Drupal.behaviors.drsneolimp = {
+    attach: function(context, settings) {
+      // Remove TID's onchange.
+      jQuery('body').find('#views-exposed-form-sport-kinds-page-1').find('.form-autocomplete').on('autocompleteclose', function() {
+        Drupal.drsneolimp.remove_tid();
+        $('.js-form-submit').click();
+      });
+      // Change select value on sport kinds filter
+      jQuery('body').find('#sport-kinds-filter').find('.change_value').once('change_value').on('click', function() {
+        var tid = $(this).attr("tid");
+        var selected = $('select[name="field_sport_kinds_target_id"]').val();
+        $('div[id="tid' + selected +'"]').removeClass('active');
+        if (tid == selected) {
+          tid = 'All';
+        }
+        else {
+          $(this).addClass('active');
+        }
+        $('select[name="field_sport_kinds_target_id"]').val(tid).change();
+      });
+    }
+  };
 
-  // Remove TID's onload.
-  // Drupal.drsneolimp.remove_tid();
-
-  // Remove TID's onchange.
-  jQuery('body').find('#views-exposed-form-sport-kinds-page-1').find('.form-autocomplete').on('autocompleteclose', function() {
-    Drupal.drsneolimp.remove_tid();
-    $('.js-form-submit').click();
-  });
-}
-};
-
-Drupal.drsneolimp = {
-  remove_tid: function () {
-    let field_autocomplete = jQuery('body').find('.form-autocomplete');
-    let autocomplete_tid_stripped = field_autocomplete.val().split(' ');
-    console.log(autocomplete_tid_stripped);
-    autocomplete_tid_stripped.pop();
-    let clear = autocomplete_tid_stripped.join(' ');
-    field_autocomplete.val(clear);
-  },
-};
-
-})(jQuery, Drupal, drupalSettings);
+  Drupal.drsneolimp = {
+    remove_tid: function () {
+      let field_autocomplete = jQuery('body').find('.form-autocomplete');
+      let autocomplete_tid_stripped = field_autocomplete.val().split(' ');
+      autocomplete_tid_stripped.pop();
+      let clear = autocomplete_tid_stripped.join(' ');
+      field_autocomplete.val(clear);
+    },
+  };
+})(jQuery, Drupal,drupalSettings);
